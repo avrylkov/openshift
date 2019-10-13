4f2422fbb9e643c0687a58b141af6ce977f9e02f
Rylkov-AV <arylkov70@mail.ru>
2019-10-13 10:56:48 +0300

    2

--- src/main/java/com/example/openshift/demoOpenshift/ShiftController.java
+++ src/main/java/com/example/openshift/demoOpenshift/ShiftController.java
@@ -6,19 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
-@RequestMapping(value = "/api/shift")
 public class ShiftController {
 
-    private static List<String> infos = Arrays.asList("Hello", "Alexander");
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd:MM:yyyy HH:mm:ss");
 
     @ResponseBody
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List getAll() {
-        return infos;
+    @RequestMapping(value = "/time")
+    public String getTime() {
+        return System.getProperty("os.name") +" date/time:" + LocalDateTime.now().format(formatter);
     }
 
 }
