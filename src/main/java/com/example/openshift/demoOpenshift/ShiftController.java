28460ad3c377e5063ce63385d556c55992d90fdb
Rylkov-AV <arylkov70@mail.ru>
2019-10-27 09:39:54 +0300

    11

--- src/main/java/com/example/openshift/demoOpenshift/ShiftController.java
+++ src/main/java/com/example/openshift/demoOpenshift/ShiftController.java
@@ -5,13 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class ShiftController {
 
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd:MM:yyyy HH:mm:ss");
